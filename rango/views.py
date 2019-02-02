e166d4143a9e508e828a4e9d178ed6ef44a294df
Andreas Nikolaou <2324286n@student.gla.ac.uk>
2019-02-02 21:28:13 +0000

    finished ch 6

--- rango/views.py
+++ rango/views.py
@@ -1,21 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
+
 
 def index(request):
-	# Query the database for a list of ALL categories currently stored.
-	# Order the categories by no. likes in descending order.
-	# Retrieve the top 5 only - or all if less than 5.
-	# Place the list in our context_dict dictionary
-	# that will be passed to the template engine.
 	category_list = Category.objects.order_by('-likes')[:5]
-	context_dict = {'categories': category_list}
-	# Render the response and send it back!
+	page_list = Page.objects.order_by('-views')[:5]
+	context_dict = {'categories': category_list, 'pages': page_list}
+
 	return render(request, 'rango/index.html', context_dict)
 
 
 def about (request):
 	context_dict = {'boldmessage': "Looks like hte bold message works!"}
+
 	return render(request, 'rango/about.html', context=context_dict)
 
-	#return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'> Index </a>")
+def show_category(request, category_name_slug):
+	context_dict = {}
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		pages = Page.objects.filter(category=category)
+		context_dict['pages'] = pages
+		context_dict['category'] = category
+
+	except Category.DoesNotExist:
+		context_dict['category'] = None
+		context_dict['pages'] = None
+
+	return render(request, 'rango/category.html', context_dict)
